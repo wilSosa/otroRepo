2fd9dc552f54e66203b132e5cc8e329bd3a9a108
Brayan <tronks_465@hotmail.com>
2022-10-07 11:18:17 -0500

    segundo

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
     })
     .catch( err => console.log(err));
 
+
+
+    
 function cargarDatos(){
     let listCocteles = document.getElementById('listCocteles');
     let data = '';
